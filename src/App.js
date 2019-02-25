884d4687e588bc6e49eca66b3012f7e2006ad782
Alex Cho <alexsjcho@gmail.com>
2019-02-25 15:38:04 -0800

    added new js files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React, { Component } from "react";
 import "./App.css";
+import Todos from "./components/Todos";
 
 class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: "Take out the trash",
+        completed: false
+      },
+      {
+        id: 2,
+        title: "Go to gym for 1 hour",
+        completed: true
+      },
+      {
+        id: 3,
+        title: "Stare at wall for 5 minutes",
+        completed: false
+      }
+    ]
+  };
   render() {
     return (
       <div className="App">
         <h1> Todo App</h1>
+        <Todos todos={this.state.todos} />
       </div>
     );
   }
